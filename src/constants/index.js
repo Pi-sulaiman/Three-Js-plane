ac4145b9fe7261b51b40d712bb412e141b181e84
Your Name <www.shaurya23@gmail.com>
2024-12-05 10:50:14 +0530

    First commit

---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import { shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    java,
+    python,
+    cpp
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: cpp,
+        name: "C++",
+        type: "Programming Language",
+    },
+    {
+        imageUrl: java,
+        name: "Java",
+        type: "Backend Development",
+    },
+    {
+        imageUrl: python,
+        name: "Python",
+        type: "AI/ML",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer (Internship)",
+        company_name: "Technohacks",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "June 2023 - August 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "MERN Stack Developer (Intern)",
+        company_name: "InfoSoft",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "August 2023 - October 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2024 - June 2024",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Sitto2002/',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/shrey-yadav-412324228/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: car,
+        theme: 'btn-back-red',
+        name: 'Roommate Finder',
+        description: 'Developed a web application that simplifies the search for compatible roommates by matching preferences and facilitating seamless connections.',
+        link: 'https://roomy-rose.vercel.app/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-green',
+        name: 'Chat Application',
+        description: 'A real-time chat application enabling seamless communication through instant messaging with a user-friendly interface.',
+        link: 'https://chat-app-beige-eight-15.vercel.app/login',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-blue',
+        name: 'News Application',
+        description: 'Developed a responsive News App that delivers real-time updates, categorized articles, and a seamless user experience.',
+        link: 'https://news-app-lac-six.vercel.app/',
+    },
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-pink',
+        name: 'Food Application',
+        description: 'A Text-to-Food app that simplifies food ordering through text-based interactions and helps yo to get items nearby you.',
+        link: 'https://github.com/Sitto2002/Food-App',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Text-to-Speech Convertor',
+        description: 'Developed a Text-to-Speech generator that converts written text into natural-sounding speech using advanced speech synthesis techniques.',
+        link: 'https://text-to-speech-iota-six.vercel.app/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Password Generator',
+        description: 'A web-based tool that generates secure, customizable passwords based on user-defined criteria to enhance online security',
+        link: 'https://password-generator1-sigma.vercel.app/',
+    }
+];
